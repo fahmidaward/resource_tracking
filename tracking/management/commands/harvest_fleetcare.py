5c90b93a6b6fed0cad4f66d91617ecfbccb2bade
Fahmida Ward <fahmida.ward@dbca.wa.gov.au>
2020-02-18 08:37:19 +0800

    fast api and peewee implementation update

--- tracking/management/commands/harvest_fleetcare.py
+++ tracking/management/commands/harvest_fleetcare.py
@@ -1,34 +1,18 @@
-"""
-from tracking.harvest import harvest_tracking_fleetcare
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "Runs harvest_tracking_email to harvest points"
-
-    def handle(self, *args, **options):
-        help = "Runs harvest_tracking_email to harvest points"
-
-        harvest_tracking_fleetcare()
-"""
-
-from tracking.harvest import harvest_tracking_fleetcare
+from tracking.harvest import save_fleetcare_db
 from django.core.management.base import BaseCommand
 
 import logging
 LOGGER = logging.getLogger('tracking_points')
 
 class Command(BaseCommand):
-    help = "Runs harvest_tracking_email to harvest points"
+    help = "Runs save_fleetcare_db to harvest points"
 
     def handle(self, *args, **options):
 
 
-        LOGGER.info('Harvesting Fleetcare feed')
+        LOGGER.info('Harvesting fleetcare database')
         try:
-            harvest_tracking_fleetcare()
-            print("Harvested from Fleetcare; created.")
+            print("Harvested {} from Fleetcare; created {}, updated {}, ignored {}".format(*save_fleetcare_db()))
             #LOGGER.info("Updated {} of {} scanned DFES devices".format(updated, num_records))
         except Exception as e:
             LOGGER.error(e)
-
